54d388070d085d0cacb54d3c79b62898b41b8a74
rbm822 <rmittnight@gmail.com>
2022-12-28 05:20:22 +0900

    Added a todo list!
    This will help me remember where I left off and what I would like to work on next.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-import earthquakeNotifier.domain.Location;
+import earthquakeNotifier.domain.Earthquake;
 import earthquakeNotifier.logic.QuakeLoader;
 import earthquakeNotifier.utils.APIConnector;
 
@@ -12,19 +12,17 @@ public class Main {
         try {
             APIConnector connector = new APIConnector("https://www.jma.go.jp/bosai/quake/data/list.json");
             QuakeLoader quakeLoader = new QuakeLoader(connector);
-//            for (Earthquake earthquake : quakeLoader.getEarthquakeSublist(0, 10)) {
+//            for (Earthquake earthquake : quakeLoader.getEarthquakeList(0, 10)) {
 //                System.out.println(earthquake);
 //            }
-
-
-//            for (Earthquake earthquake : quakeLoader.getEarthquakes().getEarthquakes()) {
-//                System.out.println(earthquake);
-//            }
-            for (Location location : quakeLoader.getLocationsWithEarthquakes().values()) {
-//                System.out.println(location.getEarthquakeWithMaxSeismicActivity());
-                System.out.println("There have been " + location.getNumOfEarthquakes() +
-                        " earthquakes at " + location + ".");
+            for (Earthquake earthquake : quakeLoader.getEarthquakeList()) {
+                System.out.println(earthquake);
             }
+//            for (Location location : quakeLoader.getLocationsWithEarthquakes().values()) {
+//                System.out.println(location.getEarthquakeWithMaxSeismicActivity());
+//                System.out.println("There have been " + location.getNumOfEarthquakes() +
+//                        " earthquakes at " + location + ".");
+//            }
         } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
---
+++ todo.md
@@ -0,0 +1,4 @@
+# Todo list
+## Searching
+- Search by specific location
+- Sort locations by number of earthquakes
