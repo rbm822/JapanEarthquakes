a9d530bc7023356f8dd3233ef45e28b6d32c12fd
Rob M <rmittnight@gmail.com>
2023-01-08 20:18:12 +0900

    Created constructor.

--- QuakePrinter.java
+++ QuakePrinter.java
@@ -1,4 +1,12 @@
 package earthquakes;
 
+import java.util.List;
+import java.util.Map;
+
 public class QuakePrinter {
+	private List<Earthquake> earthquakeList;
+	private Map<String, Location> earthquakeMap;
+
+	public QuakePrinter() {
+	}
 }
