d9eaa559813d4c900381bdf9c6c716d2ef197870
Rob M <rmittnight@gmail.com>
2023-01-08 13:14:56 +0900

    Finally got it to compile/run from CLI!

---
+++ Location.java
@@ -0,0 +1,47 @@
+package earthquakes;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Location {
+    private String locationName;
+    private EarthquakeContainer earthquakes;
+
+    public Location(String locationName) {
+        this.locationName = locationName;
+        this.earthquakes = new EarthquakeContainer();
+    }
+
+    public void addEarthquake(Earthquake earthquake) {
+        this.earthquakes.add(earthquake);
+    }
+
+    public List<Earthquake> getEarthquakes() {
+        return earthquakes.getEarthquakes();
+    }
+
+    public int getNumOfEarthquakes() {
+        return earthquakes.size();
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public String toString() {
+        return locationName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Location location = (Location) o;
+        return locationName.equals(location.locationName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(locationName);
+    }
+}
